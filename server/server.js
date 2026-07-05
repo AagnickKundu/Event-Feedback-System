286108dba3dfec0de4897910336667a4fafec24b
AagnickKundu <kunduaagnick@gmail.com>
2026-07-05 12:59:15 +0530

    server setup

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+
+const eventRoutes = require("./routes/eventRoutes");
+const feedbackRoutes = require("./routes/feedbackRoutes");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/events", eventRoutes);
+app.use("/api/feedback", feedbackRoutes);
+
+app.get("/", (req, res) => {
+    res.send("API Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
