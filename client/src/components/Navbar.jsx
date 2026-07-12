c6f897c0c597e105728edb0d0930c34e081a6e05
AagnickKundu <kunduaagnick@gmail.com>
2026-07-12 23:32:25 +0530

    frontend

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
+import "../styles/Navbar.css"
 
 function Navbar() {
   return (
-    <nav>
-      <h2>Event Feedback System</h2>
+    <nav className="navbar">
+      <div className="container navbar-container">
+      <h2 className="logo">Event Feedback System</h2>
 
-      <div>
-        <Link to="/">Home</Link>{" "}
-        <Link to="/events">Events</Link>{" "}
-        <Link to="/feedbacks">Feedback</Link>
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link>{" "}</li>
+        <li><Link to="/events">Events</Link>{" "}</li>
+        <li><Link to="/feedbacks">Feedback</Link></li>
+      </ul>
       </div>
     </nav>
   );
--- client/src/pages/FeedbackList.jsx
+++ client/src/pages/FeedbackList.jsx
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>FeedbackList Page</h1>;
+  return <h1>Feedback List Page</h1>;
 }
 
 export default Home;
